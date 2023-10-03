e396b38c81bb55975d4731e69c4095448832b976
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-10-03 17:12:11 +0530

    main.c

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
+    int x  = 20;
+    int y = 30;
+    int add = x + y;
+    printf("Addition of numbers are: %d", add);
     printf("Hello World");
     return 0;
 }
